a0f4e2519f6c0c732ff4a75a973b586925118d5c
francescccc <frandialga9@gmail.com>
2021-03-12 16:57:06 -0300

    try version

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 bot1: python main.py
 bot2: python bot.py
-bot3: python bot1.py
--- bot.py
+++ bot.py
@@ -51,116 +51,35 @@ print(users1)
 
 
 def reply():
-    users3 = db.child("id1").get()
-    print(users3.val())
-    global users1
-    tweets = api.mentions_timeline(users1, tweet_mode='extended')
-    print(users1, "uweo")
-    for tweet in reversed(tweets):
-        print(tweet.id, tweet.full_text)
-        tweettext = tweet.full_text
-        str = "https://t.co" in tweettext
-        tweettext = tweet.full_text
-        if str == True:
-            for status in tweets:
-                media_files = set()
-                media = status.entities.get('media', [])
-                tweettext = tweet.full_text
-                str = "https://t.co" in tweettext
-                if(len(media) > 0):             
-                    if str == True:
-                        print(tweet.full_text)
-                        media_files.add(media[0]['media_url'])
-                        for media_file in media_files:
-                            urllib.request.urlretrieve(media_file, 'original.png')
-                            original = Image.open('original.png')
-                            inverted = ImageOps.mirror(original)
-                            print(original.width)
-                            cropwidth = original.width // 2
-                            print(cropwidth)
-                            img = inverted
-                            x1 = img.width // 2
-                            y1 = 0
-                            x2 = img.width
-                            y2 = img.height
-                            cropped = img.crop((x1, y1, x2, y2))
-                            cropped.save('crop.png', quality=95)
-                            
-                            im1 = Image.open('original.png')
-                            im2 = Image.open('crop.png')
-
-                            im1.paste(cropped, (original.width // 2, 0))
-                            im1.save('result.png', quality=95)
-
-                            data = { "id1": tweet.id }
-                            users = db.update(data)
-                            users = db.child("id1").get()
-                            users1 = users.val()
-                            api.update_with_media('result.png', " @" + tweet.user.screen_name + " Pronto", in_reply_to_status_id = tweet.id)
-
-
-                
-
-
-
-                            
-
-        else:     
-            users3 = db.child("id").get()
-            print(users3.val())      
-            tweets = api.mentions_timeline(users1, tweet_mode='extended')
-            print(users1, "uweo")
-            print(tweet.id)
-            str1 = tweet.in_reply_to_status_id
-            if str1 is None:
-                api.update_status(" @" + tweet.user.screen_name + " Por favor, especifique a imagem que você quer que eu edite", in_reply_to_status_id = tweet.id)
-                print(" Por favor, especifique a imagem que você quer que eu edite")
-                data = { "id1": tweet.id }
-                users = db.update(data)
-                users = db.child("id1").get()
-                users1 = users.val()
-        
-            else:
-                status = api.get_status(tweet.in_reply_to_status_id, tweet_mode='extended')
-                print(tweet.in_reply_to_status_id)
-                print(status.full_text)
-                media_files = set()
-                media_files = set()
-                media = status.entities.get('media', [])
-                print(status.user.screen_name)
-                twitterbot = "invertbot_" in status.user.screen_name
-                twitterbot2 = "distortbott" in status.user.screen_name
-                print(twitterbot, twitterbot2)
-                if twitterbot == True:
-                    print(tweet.user.screen_name, "oi1")
-                    data = { "id1": tweet.id }
-                    users = db.update(data)
-                    users = db.child("id1").get()
-                    users1 = users.val()
-
-
-
-                else:    
-                    if twitterbot2 == True:
-                        print(tweet.user.screen_name, "oi1")
-                        data = { "id1": tweet.id }
-                        users = db.update(data)
-                        users = db.child("id1").get()
-                        users1 = users.val()
-
-                    else:    
-                        if(len(media) > 0):
-                            print(status.full_text)
+    try:
+        users3 = db.child("id1").get()
+        print(users3.val())
+        global users1
+        tweets = api.mentions_timeline(users1, tweet_mode='extended')
+        print(users1, "uweo")
+        for tweet in reversed(tweets):
+            print(tweet.id, tweet.full_text)
+            tweettext = tweet.full_text
+            str = "https://t.co" in tweettext
+            tweettext = tweet.full_text
+            if str == True:
+                for status in tweets:
+                    media_files = set()
+                    media = status.entities.get('media', [])
+                    tweettext = tweet.full_text
+                    str = "https://t.co" in tweettext
+                    if(len(media) > 0):             
+                        if str == True:
+                            print(tweet.full_text)
                             media_files.add(media[0]['media_url'])
                             for media_file in media_files:
                                 urllib.request.urlretrieve(media_file, 'original.png')
                                 original = Image.open('original.png')
                                 inverted = ImageOps.mirror(original)
-                                inverted.save('inverted.png', quality=95)
                                 print(original.width)
                                 cropwidth = original.width // 2
                                 print(cropwidth)
-                                img = Image.open('inverted.png')
+                                img = inverted
                                 x1 = img.width // 2
                                 y1 = 0
                                 x2 = img.width
@@ -171,7 +90,7 @@ def reply():
                                 im1 = Image.open('original.png')
                                 im2 = Image.open('crop.png')
 
-                                im1.paste(im2, (original.width // 2, 0))
+                                im1.paste(cropped, (original.width // 2, 0))
                                 im1.save('result.png', quality=95)
 
                                 data = { "id1": tweet.id }
@@ -180,14 +99,97 @@ def reply():
                                 users1 = users.val()
                                 api.update_with_media('result.png', " @" + tweet.user.screen_name + " Pronto", in_reply_to_status_id = tweet.id)
 
-                        else:
+
+                    
+
+
+
+                                
+
+            else:     
+                users3 = db.child("id").get()
+                print(users3.val())      
+                tweets = api.mentions_timeline(users1, tweet_mode='extended')
+                print(users1, "uweo")
+                print(tweet.id)
+                str1 = tweet.in_reply_to_status_id
+                if str1 is None:
+                    api.update_status(" @" + tweet.user.screen_name + " Por favor, especifique a imagem que você quer que eu edite", in_reply_to_status_id = tweet.id)
+                    print(" Por favor, especifique a imagem que você quer que eu edite")
+                    data = { "id1": tweet.id }
+                    users = db.update(data)
+                    users = db.child("id1").get()
+                    users1 = users.val()
+            
+                else:
+                    status = api.get_status(tweet.in_reply_to_status_id, tweet_mode='extended')
+                    print(tweet.in_reply_to_status_id)
+                    print(status.full_text)
+                    media_files = set()
+                    media_files = set()
+                    media = status.entities.get('media', [])
+                    print(status.user.screen_name)
+                    twitterbot = "invertbot_" in status.user.screen_name
+                    twitterbot2 = "distortbott" in status.user.screen_name
+                    print(twitterbot, twitterbot2)
+                    if twitterbot == True:
+                        print(tweet.user.screen_name, "oi1")
+                        data = { "id1": tweet.id }
+                        users = db.update(data)
+                        users = db.child("id1").get()
+                        users1 = users.val()
+
+
+
+                    else:    
+                        if twitterbot2 == True:
                             print(tweet.user.screen_name, "oi1")
                             data = { "id1": tweet.id }
                             users = db.update(data)
                             users = db.child("id1").get()
                             users1 = users.val()
 
-                    
+                        else:    
+                            if(len(media) > 0):
+                                print(status.full_text)
+                                media_files.add(media[0]['media_url'])
+                                for media_file in media_files:
+                                    urllib.request.urlretrieve(media_file, 'original.png')
+                                    original = Image.open('original.png')
+                                    inverted = ImageOps.mirror(original)
+                                    inverted.save('inverted.png', quality=95)
+                                    print(original.width)
+                                    cropwidth = original.width // 2
+                                    print(cropwidth)
+                                    img = Image.open('inverted.png')
+                                    x1 = img.width // 2
+                                    y1 = 0
+                                    x2 = img.width
+                                    y2 = img.height
+                                    cropped = img.crop((x1, y1, x2, y2))
+                                    cropped.save('crop.png', quality=95)
+                                    
+                                    im1 = Image.open('original.png')
+                                    im2 = Image.open('crop.png')
+
+                                    im1.paste(im2, (original.width // 2, 0))
+                                    im1.save('result.png', quality=95)
+
+                                    data = { "id1": tweet.id }
+                                    users = db.update(data)
+                                    users = db.child("id1").get()
+                                    users1 = users.val()
+                                    api.update_with_media('result.png', " @" + tweet.user.screen_name + " Pronto", in_reply_to_status_id = tweet.id)
+
+                            else:
+                                print(tweet.user.screen_name, "oi1")
+                                data = { "id1": tweet.id }
+                                users = db.update(data)
+                                users = db.child("id1").get()
+                                users1 = users.val()
+
+    except:
+        pass                    
 
 
 
